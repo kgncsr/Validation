e0975e76a117d618fe4f7bded27160dc5a4e1aa4
kgncsr <69785776+kgncsr@users.noreply.github.com>
2021-01-16 14:51:50 +0300

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Validation.Models;
+
+namespace Validation.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            return View("Register",new Register() { BirthDate=DateTime.Now});
+        }
+
+        [HttpPost]
+        public IActionResult Register(Register m)
+        {
+            if (string.IsNullOrEmpty(m.Name))
+            {
+                ModelState.AddModelError("Name", "İsim Zorunlu Alan");
+            }
+
+            if(string.IsNullOrEmpty(m.UserName))
+            {
+                ModelState.AddModelError("UserName", "Zorunlu Alan");
+            }
+            else
+            {
+                if (m.UserName.Length < 3)
+                {
+                    ModelState.AddModelError("UserName", "Kullanıcı Adı Minumum 3 Karakter");
+                }
+            }
+
+            if (string.IsNullOrEmpty(m.Email))
+            {
+                ModelState.AddModelError("Email", "Email Zorunlu Alan");
+            }
+            else
+            {
+                if (!m.Email.Contains("@"))
+                {
+                    ModelState.AddModelError("Email", "Email düzgün girilmemiş");
+                }
+            }
+
+            if (string.IsNullOrEmpty(m.Password))
+            {
+                ModelState.AddModelError("Password", "Parola Zorunlu Alan");
+            }
+            else
+            {
+                if (m.Password.Length < 8)
+                {
+                    ModelState.AddModelError("Password", "Parola minumum 8 karakter olmalı");
+                }
+            }
+
+            if (!m.Accepted)
+            {
+                ModelState.AddModelError("Accepted", "Şartları Kabul Etmelisiniz");
+            }
+
+
+
+
+            if (ModelState.IsValid)
+            {
+                return View("Completed", m);
+            }
+
+            return View(m);
+
+          
+        }
+
+
+    }
+}
---
+++ Models/Register.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Validation.Models
+{
+    public class Register
+    {
+        public string Name { get; set; }
+
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Parola Tekrar Alanı Yanlış")]
+        [Display(Name = "Password Again")]
+        public string RePassword { get; set; }
+
+        [UIHint("Date")]
+        public DateTime BirthDate { get; set; }
+
+        public bool Accepted{ get; set; }
+    }
+}
